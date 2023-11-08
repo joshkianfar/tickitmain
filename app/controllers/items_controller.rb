3ffff72352df0ab8ee6de48b645f02cdbcad82ef
joshkianfar <148338449+joshkianfar@users.noreply.github.com>
2023-11-08 18:14:45 +0000

    final

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,16 @@ class ItemsController < ApplicationController
 
   # DELETE /items/1 or /items/1.json
   def destroy
+    @item = Item.find(params[:id])
+
+    # Start a transaction to ensure data integrity
+    ActiveRecord::Base.transaction do
+      @item.tickets.each do |ticket|
+        user = ticket.user
+        user.update!(wallet_balance: user.wallet_balance + 0.5)
+      end
+    end
+
     @item.destroy
 
     respond_to do |format|
